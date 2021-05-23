a88ed8e3fe98160da06ba104a0e958ae4c32f8c4
arbitraryrw <nickcucakovic@gmail.com>
2021-05-23 10:46:23 +0100

    Ability to specify CLI flags via argparse added to script entrypoint

--- run.py
+++ run.py
@@ -1,7 +1,26 @@
 import os
+import argparse
 from elrondplayground.core.core import Core
 from elrondplayground.config.config import Config
 
+def handle_cli_arguments():
+    parser = argparse.ArgumentParser(
+        description='Sample project to demonstrate interacting with the Elrdon blockchain'
+    )
+
+    parser.add_argument(
+        '-a',
+        '--address', 
+        type=str,
+        required=False,
+        help='The address to handle'
+    )
+
+    args = parser.parse_args()
+
+    if args.address is not None:
+        Config.ADDRESS = args.address
+        
 
 def run():
     Config.ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,4 +31,5 @@ def run():
     c.start()
 
 if __name__ == '__main__':
+    handle_cli_arguments()
     run()
